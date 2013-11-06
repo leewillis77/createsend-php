9b21c7b902360092b581a25dc0d94efb53dd766e
Lee Willis <leewillis77@gmail.com>
2013-11-06 20:29:53 +0000

    Avoid redeclaring classes

--- class/log.php
+++ class/log.php
@@ -4,16 +4,19 @@ define('CS_REST_LOG_WARNING', 500);
 define('CS_REST_LOG_ERROR', 250);
 define('CS_REST_LOG_NONE', 0);
 
-class CS_REST_Log {
-    var $_level;
+if ( ! class_exists( 'CS_REST_Log' ) ) {
 
-    function CS_REST_Log($level) {
-        $this->_level = $level;
-    }
+	class CS_REST_Log {
+	    var $_level;
 
-    function log_message($message, $module, $level) {
-        if($this->_level >= $level) {
-            echo date('G:i:s').' - '.$module.': '.$message."<br />\n";
-        }
-    }
+	    function CS_REST_Log($level) {
+	        $this->_level = $level;
+	    }
+
+	    function log_message($message, $module, $level) {
+	        if($this->_level >= $level) {
+	            echo date('G:i:s').' - '.$module.': '.$message."<br />\n";
+	        }
+	    }
+	}
 }
